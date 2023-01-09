f69ecac1e18e34944223db6c1765b2a6d458cadf
Raimund Krämer <kraemer_raimund@web.de>
2023-01-09 18:48:17 +0100

    Optionally provide the ISBN as command line argument
    Alternatively, the ISBN can be hardcoded in the main method.

--- Java/src/main/java/org/example/Main.java
+++ Java/src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.assertj.core.util.Strings;
 import org.example.domain.BookRepository;
 import org.example.persistence.FakeInMemoryBookRepository;
 import org.example.presentation.UserInterface;
@@ -10,6 +11,15 @@ public class Main {
     private static final UserInterface userInterface = new UserInterface(bookRepository);
 
     public static void main(String[] args) {
-        userInterface.searchBook("978-3-16-148410-0");
+        if (isCommandLineArgumentProvided(args)) {
+            userInterface.searchBook(args[0]);
+        } else {
+            // Provide the ISBN here if you do not want to use the command line arguments.
+            userInterface.searchBook("978-3-16-148410-0");
+        }
+    }
+
+    private static boolean isCommandLineArgumentProvided(String[] args) {
+        return args.length > 0 && !Strings.isNullOrEmpty(args[0]);
     }
 }
