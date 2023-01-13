882d972d4dcfb18fd873cc58e39e330e2fb98dc3
Raimund Krämer <kraemer_raimund@web.de>
2023-01-13 22:01:09 +0100

    Implement "legacy code" to compare ISBNs when searching
    The comparison compares the ISBNs after removing all separators from
    both, since the input has already been validated in the presentation
    layer, and thus both ISBNs contain only spaces or only hyphens as
    separators, which are considered equivalent.

--- Java/src/main/java/org/example/persistence/FakeInMemoryBookRepository.java
+++ Java/src/main/java/org/example/persistence/FakeInMemoryBookRepository.java
@@ -2,22 +2,26 @@ package org.example.persistence;
 
 import org.example.domain.Book;
 import org.example.domain.BookRepository;
+import org.example.utils.StringUtils;
 
-import java.util.Map;
+import java.util.List;
 import java.util.Optional;
 
 public class FakeInMemoryBookRepository implements BookRepository {
 
-    private final Map<String, Book> bookByIsbn = Map.of(
-            "978-3-16-148410-0", new Book("978-3-16-148410-0", "Example Book", "Jane Doe")
+    private final List<Book> allBooks = List.of(
+            new Book("978-3-16-148410-0", "Example Book", "Jane Doe")
     );
 
     @Override
     public Optional<Book> byIsbn(String isbn) {
-        if (!bookByIsbn.containsKey(isbn)) {
-            return Optional.empty();
-        }
+        return allBooks
+                .stream()
+                .filter(book -> isEquivalentIsbn(book.getIsbn(), isbn))
+                .findFirst();
+    }
 
-        return Optional.of(bookByIsbn.get(isbn));
+    private boolean isEquivalentIsbn(String isbn1, String isbn2) {
+        return StringUtils.removeSeparators(isbn1).equals(StringUtils.removeSeparators(isbn2));
     }
 }
